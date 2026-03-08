a108084f12543b8ec678fbe541f02b6016d608d9
Godson Ajodo <godsonajodo2020@gmail.com>
2026-03-07 21:11:17 -0800

    test the crypto functions

--- steg/src/main.rs
+++ steg/src/main.rs
@@ -1,3 +1,21 @@
+pub mod crypto;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let plaintext = b"Hello, world!";
+    let password = "super_secret_password";
+
+    // Encrypt the plaintext
+    let encrypted = crypto::encrypt_with_password(plaintext, password).unwrap();
+
+    // Decrypt the ciphertext
+    let decrypted = crypto::decrypt_with_password(&encrypted, password).unwrap();
+
+    print("Plain password: {}", password);
+    print("Plaintext: {}", String::from_utf8_lossy(plaintext));
+    print("Ciphertext (hex): {}", hex::encode(&encrypted.ciphertext));
+    print("Decrypted text: {}", String::from_utf8_lossy(&decrypted));
+
+    assert_eq!(plaintext.to_vec(), decrypted);
 }
