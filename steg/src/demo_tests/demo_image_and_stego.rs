87c73c2a5c1e20f3e47357b236f4e3782834bd6b
Godson Ajodo <godsonajodo2020@gmail.com>
2026-03-14 21:33:57 -0700

    demo image and stego

--- steg/src/demo_tests/demo_image_and_stego.rs
+++ steg/src/demo_tests/demo_image_and_stego.rs
@@ -1,11 +1,6 @@
-use super::scripts::utils::approx_eq_array2;
-use super::scripts::{crypto, image_ops, stego};
-use super::scripts::crypto::decrypt_payload;
-use super::scripts::main::{extract_bytes_from_image};
+use crate::scripts::{crypto, image_ops, stego, utils};
 
-
-
-fn demo_image_and_stego(password: &str, encrypted: &[u8]) {
+pub fn demo_image_and_stego(password: &str, encrypted: &[u8]) {
     std::fs::create_dir_all("output").unwrap();
 
     let img = image_ops::load_image("data/cat.jpg").unwrap();
@@ -15,7 +10,7 @@ fn demo_image_and_stego(password: &str, encrypted: &[u8]) {
 
     let blocks = image_ops::split_into_blocks(&matrix);
     let rebuilt = image_ops::merge_blocks(&blocks, height, width);
-    assert!(approx_eq_array2(&matrix, &rebuilt, 1e-5));
+    assert!(utils::approx_eq_array2(&matrix, &rebuilt, 1e-5));
 
     gray.save("output/cat_gray.png").unwrap();
 
@@ -24,7 +19,7 @@ fn demo_image_and_stego(password: &str, encrypted: &[u8]) {
     let embedded_image = image_ops::matrix_to_gray_image(&embedded_matrix);
     embedded_image.save("output/cat_stego.png").unwrap();
 
-    let extracted_encrypted = extract_bytes_from_image("output/cat_stego.png").unwrap();
+    let extracted_encrypted = utils::extract_bytes_from_image("output/cat_stego.png").unwrap();
     let decrypted = crypto::decrypt_payload(&extracted_encrypted, password).unwrap();
 
     println!("Recovered stego text: {}", String::from_utf8_lossy(&decrypted));
