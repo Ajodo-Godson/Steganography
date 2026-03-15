92f12bddb4517dc0913cf4fc991bdec253549637
Godson Ajodo <godsonajodo2020@gmail.com>
2026-03-14 21:34:24 -0700

    utils for assert and embed

--- steg/src/scripts/utils.rs
+++ steg/src/scripts/utils.rs
@@ -1,4 +1,8 @@
-fn approx_eq_vec(left: &[f32], right: &[f32], epsilon: f32) -> bool {
+use ndarray::Array2;
+
+use crate::scripts::{image_ops, stego};
+
+pub fn approx_eq_vec(left: &[f32], right: &[f32], epsilon: f32) -> bool {
     left.len() == right.len()
         && left
             .iter()
@@ -6,9 +10,42 @@ fn approx_eq_vec(left: &[f32], right: &[f32], epsilon: f32) -> bool {
             .all(|(a, b)| (a - b).abs() <= epsilon)
 }
 
-fn approx_eq_array2(left: &Array2<f32>, right: &Array2<f32>, epsilon: f32) -> bool {
+pub fn approx_eq_array2(left: &Array2<f32>, right: &Array2<f32>, epsilon: f32) -> bool {
     left.dim() == right.dim()
         && left
             .indexed_iter()
             .all(|((r, c), v)| (*v - right[(r, c)]).abs() <= epsilon)
+}
+
+pub fn embed_bytes_into_image(input: &str, output: &str, encrypted: &[u8]) -> Result<(), String> {
+    let img = image_ops::load_image(input).map_err(|e| e.to_string())?;
+    let gray = image_ops::extract_grayscale(&img);
+    let matrix = image_ops::gray_image_to_matrix(&gray);
+    let (height, width) = matrix.dim();
+    let blocks = image_ops::split_into_blocks(&matrix);
+
+    let bits_needed = 32 + encrypted.len() * 8;
+    if bits_needed > blocks.len() {
+        return Err(format!(
+            "Payload too large for cover image: need {} bits, have {}",
+            bits_needed,
+            blocks.len()
+        ));
+    }
+
+    let embedded_blocks = stego::embed_payload_in_blocks(&blocks, encrypted)?;
+    let embedded_matrix = image_ops::merge_blocks(&embedded_blocks, height, width);
+    let embedded_image = image_ops::matrix_to_gray_image(&embedded_matrix);
+
+    embedded_image.save(output).map_err(|e| e.to_string())?;
+    Ok(())
+}
+
+pub fn extract_bytes_from_image(input: &str) -> Result<Vec<u8>, String> {
+    let stego_img = image_ops::load_image(input).map_err(|e| e.to_string())?;
+    let stego_gray = image_ops::extract_grayscale(&stego_img);
+    let stego_matrix = image_ops::gray_image_to_matrix(&stego_gray);
+    let stego_blocks = image_ops::split_into_blocks(&stego_matrix);
+
+    stego::extract_payload_from_blocks(&stego_blocks)
 }
